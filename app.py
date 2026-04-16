5a826454360d13920e287272f21a2c844e6c9a70
ejjigirikavya <mailtokavyaejjigiri@gmail.com>
2026-04-16 22:39:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from pptx import Presentation
 import difflib
+import os
 
 app = Flask(__name__)
 
@@ -126,4 +127,4 @@ def analyze():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
